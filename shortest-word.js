58b920dfaf9aa220637b2cb4435d470e343ffe57
Leonardo Galante <lndgalante@gmail.com>
2017-09-14 19:55:54 -0300

    Add first kata 🥋

---
+++ shortest-word.js
@@ -0,0 +1,15 @@
+/* 
+  Instructions ⚔️
+  Simple, given a string of words, return the length of the shortest word(s).
+  String will never be empty and you do not need to account for different data types. 
+*/
+
+function findShort(s) {
+  const wordsArray = s.split(' ')
+  const wordsLength = wordsArray.map(word => word.length)
+  const minWordLength = Math.min(...wordsLength)
+  return minWordLength
+}
+
+findShort('bitcoin take over the world maybe who knows perhaps') // -> It should return 3
+findShort('turns out random test cases are easier than writing out basic ones') // -> It should return 3
