314111e3a2652ef254da3f1f3812ce2d860f2730
Leonardo Galante <lndgalante@gmail.com>
2017-09-20 04:20:28 -0300

    Add Camel Case

---
+++ camel-case.js
@@ -0,0 +1,22 @@
+/* 
+  Description ⚔️
+    Write simple .camelCase method (camel_case function in PHP or CamelCase in C#) for strings. All words must have their first letter capitalized without spaces.
+
+  Examples
+    "hello case".camelCase() => HelloCase
+    "camel case word".camelCase() => CamelCaseWord
+  
+*/
+
+String.prototype.camelCase = function() {
+  return this.split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join('')
+}
+
+// Test to pass
+console.log('test case'.camelCase()) // => It should return "TestCase"
+console.log('camel case method'.camelCase()) // => It should return "CamelCaseMethod"
+console.log('say hello '.camelCase()) // => It should return "SayHello"
+console.log(' camel case word'.camelCase()) // => It should return "CamelCaseWord"
+console.log(''.camelCase()) // => It should return ""
--- digital-root.js
+++ digital-root.js
@@ -1,5 +1,5 @@
 /* 
-  Description: 
+  Description ⚔️
     In this kata, you must create a digital root function.
 
     A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has two digits, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
@@ -17,6 +17,7 @@ function digitalRoot(n) {
   return digitalRoot(total)
 }
 
+// Test to pass
 console.log(digitalRoot(4)) // => It should return 4
 console.log(digitalRoot(16)) // => It should return 7
 console.log(digitalRoot(942)) // => It should return 6
