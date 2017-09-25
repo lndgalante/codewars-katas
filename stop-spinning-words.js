21591ba26edcb5af64b263d033f0688bff954115
Leonardo Galante <lndgalante@gmail.com>
2017-09-25 17:35:49 -0300

    Add Stop gninnipS My sdroW!

---
+++ stop-spinning-words.js
@@ -0,0 +1,31 @@
+/* 
+  Description ⚔️
+    Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+
+  Examples:
+    spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+    spinWords( "This is a test") => returns "This is a test" 
+    spinWords( "This is another test" )=> returns "This is rehtona test"
+*/
+
+// Long solution
+function spinWords(str) {
+  return str
+    .split(' ')
+    .map(
+      word =>
+        word.length > 4
+          ? word
+              .split('')
+              .reverse()
+              .join('')
+          : word
+    )
+    .join(' ')
+}
+
+// Test to pass
+console.log(spinWords('Hey fellow warriors')) // => It should return "Hey wollef sroirraw"
+console.log(spinWords('This is a test')) // => It should return "This is a test"
+console.log(spinWords('This is another test')) // => It should return "This is rehtona test"
