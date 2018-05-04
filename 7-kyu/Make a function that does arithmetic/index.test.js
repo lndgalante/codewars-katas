53af1b893a22973ff55b3d0db91aea9d5d4d6c8c
Leonardo Galante <lndgalante@gmail.com>
2018-05-04 14:15:26 -0300

    Add new katas

---
+++ 7-kyu/Make a function that does arithmetic/index.test.js
@@ -0,0 +1,17 @@
+const arithmetic = require('./index.js')
+
+test('Test 1', () => {
+  expect(arithmetic(1, 2, 'add')).toBe(3)
+})
+
+test('Test 2', () => {
+  expect(arithmetic(8, 2, 'subtract')).toBe(6)
+})
+
+test('Test 3', () => {
+  expect(arithmetic(5, 2, 'multiply')).toBe(10)
+})
+
+test('Test 4', () => {
+  expect(arithmetic(8, 2, 'divide')).toBe(4)
+})
